43dc30ed2e99e8d2da4a5fef8ec812929e9ee170
Abhinav Thakur <thakurabhinav16160@gmail.com>
2025-09-06 21:23:14 +0530

    add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
-import DashBoard from "./pages/DashBoard";
+import Dashboard from "./pages/Dashboard";
 import WriteArticle from "./pages/WriteArticle";
 import BlogTitles from "./pages/BlogTitles"
 import GenerateImages from "./pages/GenerateImages";
@@ -23,7 +23,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path='/ai' element={<Layout/>}>
-          <Route index element={<DashBoard/>}/>
+          <Route index element={<Dashboard/>}/>
           <Route path='write-article' element={<WriteArticle/>}/>
           <Route path='blog-titles' element={<BlogTitles/>}/>
           <Route path='generate-images' element={<GenerateImages/>}/>
